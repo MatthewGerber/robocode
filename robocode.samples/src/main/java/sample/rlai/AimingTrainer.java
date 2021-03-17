afa42f6a49a9bb18a4e17fc210644817d6db1bc5
Matthew Gerber <matthew.gerber@dexcom.com>
2021-03-16 21:43:19 -0400

    Convert to dedicated aiming trainer, which gets progressively more difficult as rounds go by.

---
+++ robocode.samples/src/main/java/sample/rlai/AimingTrainer.java
@@ -0,0 +1,49 @@
+package sample.rlai;
+
+
+import robocode.AdvancedRobot;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+
+
+/**
+ * Aiming trainer.
+ * <p>
+ * This robot moves around in increasingly complex ways, to train an aiming control agent.
+ *
+ * @author Matthew S. Gerber (original)
+ */
+public class AimingTrainer extends AdvancedRobot {
+
+	boolean movingForward;
+
+	public void run() {
+
+		movingForward = true;
+
+		while (true) {
+			if (movingForward) {
+				ahead(getRoundNum());
+			} else {
+				back(getRoundNum());
+			}
+			for (int i = 0; i < 500 - getRoundNum(); ++i) {
+				doNothing();
+			}
+		}
+	}
+
+	public void onHitWall(HitWallEvent e) {
+		reverseDirection();
+	}
+
+	public void reverseDirection() {
+		movingForward = !movingForward;
+	}
+
+	public void onHitRobot(HitRobotEvent e) {
+		if (e.isMyFault()) {
+			reverseDirection();
+		}
+	}
+}
