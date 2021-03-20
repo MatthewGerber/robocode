5188ffac7193f71f663a23b8019d3799e65c5c8c
Matthew Gerber <matthew.gerber@dexcom.com>
2021-03-20 16:42:06 -0400

    Slower movement.

--- robocode.samples/src/main/java/sample/rlai/AimingTrainer.java
+++ robocode.samples/src/main/java/sample/rlai/AimingTrainer.java
@@ -27,7 +27,7 @@ public class AimingTrainer extends AdvancedRobot {
 			} else {
 				back(5);
 			}
-			for (int i = 0; i < 40 - getRoundNum(); ++i) {
+			for (int i = 0; i < 120 - getRoundNum(); ++i) {
 				doNothing();
 			}
 		}
